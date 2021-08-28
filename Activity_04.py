84a261f4122da9a3633c99f6a93b1971c8481b0c
abhyudaycm <83929439+abhyudaycm@users.noreply.github.com>
2021-08-28 17:36:38 +0530

    Update Activity_04.py

---
+++ Activity_04.py
@@ -0,0 +1,9 @@
+
+a = int( input ("Enter the first number to be added: \n" ))
+                
+b = int( input ("Enter the second number to be added: \n"))
+                
+c = a + b
+                
+print("The sum of %d and %d is %d\n " %(a, b, c))
+
