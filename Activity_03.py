365b4a58e714824f769a1f08eb53737d74fbd96a
abhyudaycm <83929439+abhyudaycm@users.noreply.github.com>
2021-08-28 12:46:21 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,17 @@
+
+str1 = input ("enter string 1 : ")
+str2 = input ("enter string 2 : ")
+
+str3 = str1 + str2
+
+print (str3)
+
+
+
+str1 = input ("enter string 1 : \n ")
+str2 = input ("enter string 2 : \n ")
+
+str3 = str1 + str2
+
+print (str3)
+
